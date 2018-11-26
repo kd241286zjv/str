805184d937991f7b74d1b2bdf9fd4acc6d073c18
unknown <kd241286zjv@gmail.com>
2018-11-26 23:09:20 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
-  
-})
+$(document).ready(function() {
+  $('.scrollDown').click(function() {
+    var destination = $('.benefitTermsSection').offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 300);
+    return false;
+  });
+});
